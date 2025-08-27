512fb0c38264bf851ff61160e2b02ac13ed5c76e
coder-krishna-00 <thisiskrishna00@gmail.com>
2025-08-27 12:45:54 +0530

    Basic python code file.

---
+++ basics.py
@@ -0,0 +1,37 @@
+# print("Hello World")
+
+# if (5 > 2):
+#     print("five is bigger!")
+
+# x = 3
+# y = 5
+# z = "John"
+# if (x > y):
+#     print ("hello")
+# else:
+#     print ("You are now right!")
+
+# print(type(z))
+
+# x, y, z = 5,"john", 6.9
+
+# print(y)
+
+# fruits = ["Orange", "Bananna"]
+
+# print(fruits, type(fruits))
+
+# marvel = ("Spidey", "Thor")
+# print(marvel,type(marvel))
+
+# x = "Ironman"
+# y = "Blasters"
+
+# def weapons():
+#     global x
+#     x = "Spiderman "
+#     y = " webshooters"
+#     print(x + "uses" + y)
+
+# weapons()
+# print (x)
